5cb2afdd3273fca50ce7ecfc9743649d0162ff7c
Joseph Yull <jyull1@ithaca.edu>
2016-05-20 21:04:48 -0400

    Added Resources Folder w/ Test Payroll Report

--- README.md
+++
@@ -1,2 +0,0 @@
-# PayrollManager
-A parser &amp; checker for Ithaca College's DIIS Student payroll reports.
--- src/Worker.java
+++ src/Worker.java
@@ -2,8 +2,13 @@
  * Created by Joseph on 5/19/2016.
  */
 public class Worker {
+    /**Number of shifts the employee works. Shifts that occur on the same day,
+     * but separated by any amount of time, should be counted as 2 shifts.*/
     private int shifts = 0;
+
+    /**String of "expected" employee shifts; i.e. the ideal punch-in/punch-out time
+     * for each shift.*/
     private String[] shiftDetails;
 
-    //TODO: Add JSON to facilitate shift creation.
+    //TODO: Add append "Shift String" method for shiftDetails.
 }

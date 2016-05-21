c97b4b17404f472e3fbb8345c7fa4b7b99aa3425
Joseph Yull <jyull1@ithaca.edu>
2016-05-20 20:45:05 -0400

    Added Default Intellij Project Structure

---
+++ src/Worker.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Joseph on 5/19/2016.
+ */
+public class Worker {
+    private int shifts = 0;
+    private String[] shiftDetails;
+
+    //TODO: Add JSON to facilitate shift creation.
+}
